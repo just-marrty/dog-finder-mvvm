89af7ef2d27141c07120d344663f8ec22039e79b
Martin <marrty.h.1992@gmail.com>
2025-12-08 20:22:35 +0100

    Initial Commit

---
+++ DogFinder/DogFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogFinderApp.swift
+//  DogFinder
+//
+//  Created by Martin Hrbáček on 08.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct DogFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
